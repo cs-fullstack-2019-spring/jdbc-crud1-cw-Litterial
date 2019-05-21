037bf9d9b8281a1b9fbaeae9b9dd479903e84954
Litterial <yungice12@gmail.com>
2019-05-20 23:46:13 -0500

    cw

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,19 @@ public class Main {
         }
         try {
             conn = java.sql.DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
-            insertEmployees(conn);
-
+            System.out.println("Connected to the PostgreSQL server successfully.\n");
+            System.out.println("Exercise 1");
+            selectEmployees(conn);
+            System.out.println("\n Exercise 2");
+            selectDevelopers(conn,"Developer");
+            System.out.println("\n Exercise 3");
+            over50000(conn,50000);
+//            System.out.println("\n Exercise 4");
+//            newEngineer(conn,"Thomas","Tank","Engineer",50000);
+//            System.out.println("\nExercise 5");
+//            updateEllen(conn,42000);
+//            System.out.println("\nExercise 7");
+//            deleteSam(conn);
         } catch (java.sql.SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -23,9 +33,15 @@ public class Main {
         return conn;
     }
 
-    public static void insertEmployees(Connection conn)
+    public static void selectEmployees(Connection conn)
     {
 //        String SQL = "Insert into employees(first_name, last_name, position, salary) Values('Chuck','Jones','Manager',100000)";
+//        String SQL = "Insert into employees(first_name, last_name, position, salary) Values('Marty', 'Krofft', 'Engineer', 80000)";
+//        String SQL = "Insert into employees(first_name, last_name, position, salary) Values('Peter', 'Barker', 'Developer', 50000)";
+//        String SQL = "Insert into employees(first_name, last_name, position, salary) Values('Ellen', 'Musk', 'Developer', 38000)";
+//        String SQL = "Insert into employees(first_name, last_name, position, salary) Values('Sam', 'Sham', 'Developer', 35000)";
+
+
         String SQL = "Select * from employees";
         try {
             PreparedStatement pstmt = conn.prepareStatement(SQL);
@@ -42,6 +58,108 @@ public class Main {
             System.out.println(ex.getMessage());
         }
 
+    }
+
+    public static void selectDevelopers(Connection conn,String job)
+    {
+
+        String SQL = "Select * from employees  where position=?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1,job);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                //returns element at column index
+                System.out.println(rs.getInt(1)+","+rs.getString(2) + ","
+                        + rs.getString(3) + ","+rs.getString(4) + ","+rs.getInt(5));
+            }
+        }
+
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    public static void over50000(Connection conn,int money)
+    {
+
+        String SQL = "Select * from employees  where salary >?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1,money);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                //returns element at column index
+                System.out.println(rs.getInt(1)+","+rs.getString(2) + ","
+                        + rs.getString(3) + ","+rs.getString(4) + ","+rs.getInt(5));
+            }
+        }
+
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    public static void newEngineer(Connection conn,String fname, String lname, String job, int salary)
+    {
+
+        String SQL = "Insert into employees(first_name, last_name, position, salary) Values('Thomas','Tank','Engineer',75000)";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                //returns element at column index
+                System.out.println(rs.getInt(1)+","+rs.getString(2) + ","
+                        + rs.getString(3) + ","+rs.getString(4) + ","+rs.getInt(5));
+            }
+        }
+
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    public static void updateEllen(Connection conn,int money)
+    {
+
+        String SQL = "Update employees set salary =? where first_name='Ellen' and last_name='Musk' ";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setInt(1,money);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                //returns element at column index
+                System.out.println(rs.getInt(1)+","+rs.getString(2) + ","
+                        + rs.getString(3) + ","+rs.getString(4) + ","+rs.getInt(5));
+            }
+        }
+
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    public static void deleteSam(Connection conn)
+    {
+        String SQL = "Delete from employees  where first_name='Sam' and last_name='Sham' ";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                //returns element at column index
+                System.out.println(rs.getInt(1)+","+rs.getString(2) + ","
+                        + rs.getString(3) + ","+rs.getString(4) + ","+rs.getInt(5));
+            }
+        }
+
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 
 
